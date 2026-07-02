c96192feaf64ec1a5b8e76ac2d2e861810256738
Gokul SAJEEV <gokulpsajeev@gmail.com>
2026-07-02 10:48:29 +0530

    Add wing loading calculation

---
+++ calculations.py
@@ -0,0 +1,13 @@
+# ==========================================
+# Aircraft Performance Calculations
+# ==========================================
+
+def wing_loading(weight, wing_area):
+    """
+    Calculate Wing Loading
+
+    Formula:
+    Wing Loading = Weight / Wing Area
+    """
+
+    return weight / wing_area
